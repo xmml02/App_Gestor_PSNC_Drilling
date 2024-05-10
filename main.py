e0dcdfa8e403cf2c7df514d9312c8ab0faf303eb
mariano <mlorenzo@pan-energy.com>
2024-05-10 17:08:09 -0300

    v3 Prueba

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import tkinter as tk
 from tkinter import messagebox
+import subprocess
+from datetime import datetime
 
 def main():
+
+    listCommits = get_git_commits()
+
     root = tk.Tk()
     root.title("Prueba PSNC")
     root.geometry("400x400")
     root.config(bg="lightblue")
 
+    # etiqueta ultima version
+    label = tk.Label(root, text="Ultima version: "+listCommits[0]['denominacion'], font=("Arial", 10), bg="lightblue")
+    label.pack(pady=20)
+
     #creamos una etiqueta
-    label = tk.Label(root, text="Bienvenido a mi programa", font=("Arial", 20), bg="lightblue")
+    label = tk.Label(root, text="VERSION DE PRUEBA", font=("Arial", 20), bg="lightblue")
     label.pack(pady=20)
 
     #creamos un boton
@@ -17,20 +26,31 @@ def main():
 
     root.mainloop()
 
-import subprocess
+def get_git_commits():
+    commit_info = subprocess.check_output(['git', 'log', '--pretty=format:%h - %ad']).decode('utf-8').strip()
+    commit_list = commit_info.split('\n')
+    commit_dicts = []
+    for commit in commit_list:
+        parts = commit.split(' - ')
+        try:
+            version = subprocess.check_output(['git', 'show', '-s', '--pretty=format:%s', parts[0]]).decode(
+                'utf-8').strip()
+        except subprocess.CalledProcessError:
+            version = 's/n'
+        commit_dict = {
+            'rev-parse': parts[0],
+            'denominacion': version,
+            'fecha': datetime.strptime(parts[1], '%a %b %d %H:%M:%S %Y %z')  # convert string to datetime object
+        }
+        commit_dicts.append(commit_dict)
 
-def get_git_revision_hash():
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+    # sort the commits by date in descending order
+    commit_dicts = sorted(commit_dicts, key=lambda x: x['fecha'], reverse=True)
 
-def get_git_revision_short_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
-
-
-
+    return commit_dicts
 
 if __name__ == "__main__":
-    print(get_git_revision_hash())
-    print(get_git_revision_short_hash())
+
     main()
 
 
